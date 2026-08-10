98d11511a6e4e5958bbd466ed59cbd2e42703380
Nath <nath_gabito@dlsu.edu.ph>
2026-08-10 20:44:42 +0800

    (Nath) Added SUV model

--- src/main/java/ph/dlsu/edu/lbycpob/model/SUV.java
+++ src/main/java/ph/dlsu/edu/lbycpob/model/SUV.java
@@ -1,4 +1,24 @@
 package ph.dlsu.edu.lbycpob.model;
 
-public class SUV {
+import java.util.List;
+
+/**
+ * INHERITANCE + POLYMORPHISM: another Vehicle subtype with its own set of
+ * default parts, selected purely by which class was instantiated.
+ */
+public class SUV extends Vehicle {
+
+    public SUV(String brand, String model, String plateNumber, String ownerName, String contactNumber) {
+        super(brand, model, plateNumber, ownerName, contactNumber);
+    }
+
+    @Override
+    public String getVehicleType() {
+        return "SUV";
+    }
+
+    @Override
+    public List<String> getDefaultParts() {
+        return List.of("Engine", "4WD System", "Brakes", "Suspension", "Tires", "Battery", "Transfer Case");
+    }
 }
