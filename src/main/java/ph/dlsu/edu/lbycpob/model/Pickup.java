ddaf31b7c2843a261f5c1df21afbee15cf0702b0
Nath <nath_gabito@dlsu.edu.ph>
2026-08-10 20:45:32 +0800

    (Nath) Added Pickup model

--- src/main/java/ph/dlsu/edu/lbycpob/model/Pickup.java
+++ src/main/java/ph/dlsu/edu/lbycpob/model/Pickup.java
@@ -1,4 +1,23 @@
 package ph.dlsu.edu.lbycpob.model;
 
-public class Pickup {
+import ph.dlsu.edu.lbycpob.model.Vehicle;
+
+import java.util.List;
+
+/** INHERITANCE + POLYMORPHISM: Pickup-specific parts list. */
+public class Pickup extends Vehicle {
+
+    public Pickup(String brand, String model, String plateNumber, String ownerName, String contactNumber) {
+        super(brand, model, plateNumber, ownerName, contactNumber);
+    }
+
+    @Override
+    public String getVehicleType() {
+        return "Pickup";
+    }
+
+    @Override
+    public List<String> getDefaultParts() {
+        return List.of("Engine", "Transmission", "Tailgate", "Suspension", "Tires", "Battery", "Tow Hitch");
+    }
 }
