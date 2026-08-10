ff1970558575ec447b4311b5d0aa7bd90d1dc9e1
Nath <nath_gabito@dlsu.edu.ph>
2026-08-10 20:46:03 +0800

    (Nath) Added Motorcycle model

--- src/main/java/ph/dlsu/edu/lbycpob/model/Motorcycle.java
+++ src/main/java/ph/dlsu/edu/lbycpob/model/Motorcycle.java
@@ -1,4 +1,21 @@
 package ph.dlsu.edu.lbycpob.model;
 
-public class Motorcycle {
+import java.util.List;
+
+/** INHERITANCE + POLYMORPHISM: Motorcycle-specific parts list. */
+public class Motorcycle extends Vehicle {
+
+    public Motorcycle(String brand, String model, String plateNumber, String ownerName, String contactNumber) {
+        super(brand, model, plateNumber, ownerName, contactNumber);
+    }
+
+    @Override
+    public String getVehicleType() {
+        return "Motorcycle";
+    }
+
+    @Override
+    public List<String> getDefaultParts() {
+        return List.of("Engine", "Chain/Sprocket", "Brakes", "Tires", "Battery", "Forks");
+    }
 }
