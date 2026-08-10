d6b67b1fd5f5a54337e3886de2d54214c0ad1d26
Nath <nath_gabito@dlsu.edu.ph>
2026-08-10 20:42:24 +0800

    (Nath) Added Van model

--- src/main/java/ph/dlsu/edu/lbycpob/model/Van.java
+++ src/main/java/ph/dlsu/edu/lbycpob/model/Van.java
@@ -1,4 +1,21 @@
 package ph.dlsu.edu.lbycpob.model;
 
-public class Van {
+import java.util.List;
+
+/** INHERITANCE + POLYMORPHISM: Van-specific parts list. */
+public class Van extends repairapp.model.Vehicle {
+
+    public Van(String brand, String model, String plateNumber, String ownerName, String contactNumber) {
+        super(brand, model, plateNumber, ownerName, contactNumber);
+    }
+
+    @Override
+    public String getVehicleType() {
+        return "Van";
+    }
+
+    @Override
+    public List<String> getDefaultParts() {
+        return List.of("Engine", "Sliding Door Track", "Brakes", "Suspension", "Tires", "Battery", "Cooling System");
+    }
 }
