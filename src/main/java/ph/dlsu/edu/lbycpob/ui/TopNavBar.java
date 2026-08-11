1e5a7cb8cf92f47813b18177eb387112acac6c02
Nath <nath_gabito@dlsu.edu.ph>
2026-08-11 19:57:45 +0800

    (Nath) Added new import package to TopNavBar

--- src/main/java/ph/dlsu/edu/lbycpob/ui/TopNavBar.java
+++ src/main/java/ph/dlsu/edu/lbycpob/ui/TopNavBar.java
@@ -1,7 +1,7 @@
 package ph.dlsu.edu.lbycpob.ui;
 
-import com.autoworks.repair.AutomationRepairApp;
-import com.autoworks.repair.util.UIUtil;
+import ph.dlsu.edu.lbycpob.AutomationRepairApp;
+import ph.dlsu.edu.lbycpob.util.UIUtil;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
