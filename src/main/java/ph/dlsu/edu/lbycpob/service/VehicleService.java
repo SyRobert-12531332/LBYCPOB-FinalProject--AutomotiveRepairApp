6e6bb8c806d96bf418529f4fa1ed6d74d7048f5d
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-08-13 20:00:34 +0800

    (Rovi)- double checked pom.xml and added VehicleService.java

--- src/main/java/ph/dlsu/edu/lbycpob/service/VehicleService.java
+++ src/main/java/ph/dlsu/edu/lbycpob/service/VehicleService.java
@@ -1,4 +1,26 @@
 package ph.dlsu.edu.lbycpob.service;
 
+import ph.dlsu.edu.lbycpob.model.Vehicle;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Handles persistence for customer vehicles (Vehicles.json).
+ */
 public class VehicleService {
+
+    private static final String FILE_NAME = "Vehicles.json";
+    private static final Type LIST_TYPE = new TypeToken<ArrayList<Vehicle>>() {
+    }.getType();
+
+    public List<Vehicle> loadVehicles() {
+        return JsonStore.load(FILE_NAME, LIST_TYPE, new ArrayList<>());
+    }
+
+    public void saveVehicles(List<Vehicle> vehicles) {
+        JsonStore.save(FILE_NAME, vehicles);
+    }
 }
